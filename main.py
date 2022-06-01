8e42b81872e492ea11d698ac5cd9dabfe17b0059
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-06-01 20:42:45 +0200

    twitter acabado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from UserAccount import *
+from classes_Twitter import *
 if __name__ == "__name__":
     user1 = UserAccount("pepe","pepe@gmail.com", [], [], [])
     user2 = UserAccount("llucia","llucia@gmail.com", [], [], [])
