94c1ef3507749a60a00f941e3994801d1e171351
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-06-01 23:15:26 +0200

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from classes_Twitter import *
-if __name__ == "__name__":
-    user1 = UserAccount("pepe","pepe@gmail.com", [], [], [])
-    user2 = UserAccount("llucia","llucia@gmail.com", [], [], [])
-
